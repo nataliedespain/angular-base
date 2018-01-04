1142e8bd6f7f1ead86950cec78e474109ba5f245
Natalie Despain <ndespain@glynlyon.com>
2018-01-04 15:40:32 -0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+
+import { AppModule } from '../app/app.module';
+
+declare var module: any;
+if (module.hot) {
+    module.hot.accept();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
